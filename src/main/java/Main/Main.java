40262ff6c264612c444fdac8295c1c0bcae44517
daae02 <65504346+daae02@users.noreply.github.com>
2022-09-01 00:20:42 -0600

    Base proyecto

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import java.io.File;
+
+/**
+ *
+ * @author DiegoAlvarez
+ */
+public class Main {
+    public static void main(String[] args) {
+        String path = "C:/Users/DiegoAlvarez/Documents/NetBeansProjects/ProyectoCompiladores/src/main/java/Lexico/Lexer.flex";
+        generateLexer(path);
+    
+    }
+    public static void generateLexer(String path){
+        File jflexFile = new File(path);
+        JFlex.Main.generate(jflexFile);
+    }
+}
