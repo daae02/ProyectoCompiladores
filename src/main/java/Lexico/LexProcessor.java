76d588c41d3387261221c6188df6d681939cddd2
daae02 <65504346+daae02@users.noreply.github.com>
2022-09-13 00:01:53 -0600

    GUI integration 2

--- src/main/java/Lexico/LexProcessor.java
+++ src/main/java/Lexico/LexProcessor.java
@@ -5,14 +5,19 @@
  */
 package Lexico;
 
+import GUI.ResultPanel;
+import GUI.ResultsPanel;
 import GUI.Upload;
+import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.BoxLayout;
 
 /**
  *
@@ -20,33 +25,90 @@ import java.util.logging.Logger;
  */
 public class LexProcessor {
     Reader read;
-    public String simpleAnalisis(String path){
+    ArrayList<Word> results;
+    ArrayList<Word> errors;
+    ResultsPanel panel;
+    private Word isRegistered(String rWord,ArrayList<Word> array){
+        for (int i = 0; i < array.size(); i++) {
+            if(array.get(i).word.equals(rWord))
+                return array.get(i);
+        }
+        return null;
+    }
+    private void addToArray(ArrayList<Word> array,Lexer lex, Tokens tok){
+        Word currentWord = isRegistered(lex.lexeme,array);
+        if (currentWord != null){
+            currentWord.addAparition(lex.line);
+        }
+        else
+            array.add(new Word(lex.lexeme,tok.toString(),lex.line));
+    }
+    private void printResults(ArrayList<Word> array){
+        for (int i = 0; i < array.size(); i++) {
+            Word cword = array.get(i);
+            System.out.println(cword.word+" "+cword.token+" "+cword.aparitions+" "+cword.printLines());
+            System.out.println("color: "+cword.col.toString());
+        }
+    }
+    public void simpleAnalisis(String path){
         try {
                 read = new BufferedReader(new FileReader(path));
                 Lexer lexer = new Lexer(read);
-                String res = "";
+                results = new  ArrayList<>();
+                errors = new  ArrayList<>();
                 while(true){
                     Tokens  tokens = lexer.yylex();
                     if (tokens == null){
-                        res += "FIN";
-                        System.out.println(res);
-                        return res;
+                        System.out.println("RESULTADOS");
+                        printResults(results);
+                        System.out.println("ERRORES");
+                        printResults(errors);
+                        showResults();
+                        showErrors();
+                        return;
                     }
-                    switch (tokens) {
-                        case ERROR:
-                            res += "Simbolo no definido\n";
-                            break;
-                        default:
-                            res +=lexer.lexeme + "|" + tokens + "|" + lexer.line + "\n";
-                            break;
-                    }                  
-
+                    else{
+                        switch (tokens) {
+                            case ERROR:
+                                addToArray(errors,lexer,tokens);
+                                break;
+                            default:
+                                addToArray(results,lexer,tokens);
+                                break;
+                        }                  
+                    }
                 }
             } catch (FileNotFoundException ex) {
                 Logger.getLogger(Upload.class.getName()).log(Level.SEVERE, null, ex);
             } catch (IOException ex) {
                 Logger.getLogger(Upload.class.getName()).log(Level.SEVERE, null, ex);
             }
-        return "Couldn't access the file";
+    }
+    private ResultPanel createPanel(Word panelWord){
+        return new ResultPanel(String.valueOf(panelWord.aparitions),panelWord.printLines(),panelWord.token,panelWord.word);
+        
+        
+    }
+    private void showResults(){
+        panel = new ResultsPanel();
+        javax.swing.JPanel cont = new javax.swing.JPanel();
+        cont.setLayout(new BoxLayout(cont, BoxLayout.Y_AXIS));
+        panel.addWords(new ResultPanel("Apariciones","Lineas","Identificador","Token"),cont);
+        for (int i = 0; i < results.size(); i++) {
+            panel.addWords(createPanel(results.get(i)),cont,results.get(i).col);
+        }
+        panel.setVisible(true);
+    }
+    private void showErrors(){
+        if (errors.size() > 0){
+            panel = new ResultsPanel();
+            javax.swing.JPanel cont = new javax.swing.JPanel();
+            cont.setLayout(new BoxLayout(cont, BoxLayout.Y_AXIS));
+            panel.addWords(new ResultPanel("Apariciones","Lineas","Identificador","Token"),cont);
+            for (int i = 0; i < results.size(); i++) {
+                panel.addWords(createPanel(results.get(i)),cont,Color.RED);
+            }
+            panel.setVisible(true);
+        }
     }
 }
