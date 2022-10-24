170a956caa02b594be499bb4759c599f531a4e93
daae02 <65504346+daae02@users.noreply.github.com>
2022-10-24 16:18:53 -0600

    Parser changes

--- src/main/java/Lexico/Tokens.java
+++ src/main/java/Lexico/Tokens.java
@@ -99,5 +99,6 @@ public enum Tokens {
     OctalFlotante,
     Hexadecimal,
     HexadecimalFlotante,
+    Reservadas,
     ERROR
 }
--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -6,18 +6,44 @@
 package Main;
 
 import java.io.File;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
     
-    public static void main(String[] args) {
-        String path = "./src/main/java/Lexico/Lexer.flex";
-        generateLexer(path);
+    public static void main(String[] args) throws Exception {
+        String ruta1 = "./src/main/java/Lexico/Lexer.flex";
+        String ruta2 = "./src/main/java/Lexico/LexerSintactico.flex";
+        String[] rutaS = {"-parser", "Sintax", "./src/main/java/Sintactico/Sintax.cup"};
+        generar(ruta1, ruta2, rutaS);
     }
-    
-    public static void generateLexer(String path){
-        File jflexFile = new File(path);
-        JFlex.Main.generate(jflexFile);
+    public static void generar(String ruta1, String ruta2, String[] rutaS) throws IOException, Exception{
+        File archivo;
+        archivo = new File(ruta1);
+        JFlex.Main.generate(archivo);
+        archivo = new File(ruta2);
+        JFlex.Main.generate(archivo);
+        System.out.println("Run cup");
+        
+        java_cup.Main.main(rutaS);
+        System.out.println("Runned cup");
+        Path rutaSym = Paths.get("./src/main/java/Sintactico/sym.java");
+        if (Files.exists(rutaSym)) {
+            Files.delete(rutaSym);
+        }
+        Files.move(
+                Paths.get("./sym.java"), 
+                Paths.get("./src/main/java/Sintactico/sym.java")
+        );
+        Path rutaSin = Paths.get("./src/main/java/Sintactico/Sintax.java");
+        if (Files.exists(rutaSin)) {
+            Files.delete(rutaSin);
+        }
+        Files.move(
+                Paths.get("./Sintax.java"), 
+                Paths.get("./src/main/java/Sintactico/Sintax.java")
+        );
     }
-    
 }
